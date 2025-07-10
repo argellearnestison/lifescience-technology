55a0b6d66556cf5a472a05f2ab8fa8fc91618c21
Argell Earnest Ison <argellearnestison@Argells-MacBook-Air.local>
2025-07-10 21:33:40 +0800

    Header and Hero section

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// Initialize Bootstrap tooltips if any
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+}); 
